b8761199da70b0c7d2cac4a7b3c1b10d2ec04dae
zhoubaoqi <zhoubaoqi@icarbonx.com>
2020-05-05 09:59:34 +0800

    vector

---
+++ collections/collections/main.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void printVector(std::vector<int>::iterator& it, std::vector<int>& v);
+
+void printVectorByIterator(std::reverse_iterator<std::vector<int>::iterator>& rbegin, std::vector<int>& v);
